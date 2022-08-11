795cf69e481e1635dc4797e97fd9eb8174b6e99e
evⁿ ☕ <gh@cli.lol>
2022-08-11 04:13:07 +0200

    Fixed.

--- experiments/index.js
+++ experiments/index.js
@@ -2,6 +2,7 @@
 // https://gist.github.com/MeguminSama/2cae24c9e4c335c661fa94e72235d4c4?permalink_comment_id=4262896#gistcomment-4262896
 
 import * as webpackModules from '@goosemod/webpack';
+import showToast from "@goosemod/toast";
 
 const settingsStore = webpackModules.findByProps("isDeveloper");
 const userStore = webpackModules.findByProps("getUsers");
@@ -20,7 +21,16 @@ export default {
         },
 
         onRemove: async () => {
-            Object.values(settingsStore._dispatcher._dependencyGraph.nodes).find(x => x.name == "ExperimentStore").actionHandler["CONNECTION_OPEN"]({user: {flags: 0}, type: "CONNECTION_OPEN"}
+            // I DON'T KNOW HOW TO REVERT IT LOL!!
+            /* const nodes = Object.values(settingsStore._dispatcher._actionHandlers._dependencyGraph.nodes);
+            try {
+                nodes.find(x => x.name == "ExperimentStore").actionHandler["CONNECTION_OPEN"]({user: {flags: 0}, type: "CONNECTION_OPEN"})
+            } catch (e) {} // this will always intentionally throw
+            const oldGetUser = userStore.__proto__.getCurrentUser;
+            userStore.__proto__.getCurrentUser = () => ({hasFlag: () => false})
+            nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]()
+            userStore.__proto__.getCurrentUser = oldGetUser */
+            showToast("REFRESH!")
         }
     }
 }
